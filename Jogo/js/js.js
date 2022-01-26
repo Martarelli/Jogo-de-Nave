4ad913867fe7a1b60f2d6ab56cbacf6a4b3dab94
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-01-26 19:58:16 -0300

    aumentando a dificuldade do jogo

--- Jogo/js/js.js
+++ Jogo/js/js.js
@@ -176,6 +176,7 @@ function start() {
     // Disparo com o inimigo1
     if (colisao3.length > 0) {
       pontos = pontos + 100;
+      velocidade=velocidade+0.6;
       inimigo1X = parseInt($("#inimigo1").css("left"));
       inimigo1Y = parseInt($("#inimigo1").css("top"));
 
